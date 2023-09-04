13fecb919155eb99f651b8dcebef09bea712fda6
JoeTheCookie217 <56717228+JoeTheCookie217@users.noreply.github.com>
2023-09-04 16:42:58 +0200

    test: init storage manually instead of calling constructor

--- assembly/__tests__/basic-tests.spec.ts
+++ assembly/__tests__/basic-tests.spec.ts
@@ -3,9 +3,10 @@ import {
   bytesToFixedSizeArray,
   bytesToI32,
   bytesToU64,
+  fixedSizeArrayToBytes,
   u64ToBytes,
 } from '@massalabs/as-types';
-import { IS_OWNER, OWNERS, REQUIRED, constructor } from '../contracts/Multisig';
+import { IS_OWNER, OWNERS, REQUIRED } from '../storage/Multisig';
 import {
   Address,
   Context,
@@ -17,33 +18,34 @@ import {
   resetStorage,
 } from '@massalabs/massa-as-sdk';
 
-// const user1 = generateDumbAddress();
-// const user2 = generateDumbAddress();
-// const user3 = generateDumbAddress();
-// const owners = [user1, user2, user3];
-const owners = ['user1', 'user2'];
+const user1 = generateDumbAddress();
+const user2 = generateDumbAddress();
+const user3 = generateDumbAddress();
+const owners = [user1, user2, user3];
 
-beforeEach(() => {
-  resetStorage();
-  mockAdminContext(true);
-  const required = 2;
-  const args = new Args().add(owners).add(required).serialize();
-  constructor(args);
-  mockAdminContext(false);
-});
-
-describe('MasterChef', () => {
+describe('Multisig', () => {
+  beforeEach(() => {
+    resetStorage();
+    // mockAdminContext(true);
+    // const required = 2;
+    // const args = new Args().add(owners).add(required);
+    // constructor(args.serialize());
+    // mockAdminContext(false);
+    Storage.set(OWNERS, fixedSizeArrayToBytes(owners));
+    Storage.set(REQUIRED, u64ToBytes(2));
+    for (let i = 0; i < owners.length; i++) {
+      IS_OWNER.set(owners[i], true);
+    }
+  });
   it('Should be correctly initialized', () => {
-    generateEvent('checking some shit');
-    generateEvent(Storage.has(OWNERS).toString());
     const _owners = bytesToFixedSizeArray<string>(Storage.get(OWNERS));
     expect(_owners.length).toBe(3);
 
     const _required = bytesToI32(Storage.get(REQUIRED));
     expect(_required).toBe(2);
 
-    // const isOwner = IS_OWNER.getSome(user1);
-    // expect(isOwner).toBe(true);
+    const isOwner = IS_OWNER.get(user1, false);
+    expect(isOwner).toBe(true);
   });
 });
 
